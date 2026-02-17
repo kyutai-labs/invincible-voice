79ffbf385166a54db856558171902faf2a867dc7
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-02-17 12:12:13 +0100

    Remove comment

--- services/backend/backend/libs/health.py
+++ services/backend/backend/libs/health.py
@@ -1,7 +1,7 @@
 from backend.typing import HealthStatus
 
 
-async def get_health():  # dummy param _none because caching function expects a single param as cache key.
+async def get_health():
     return HealthStatus(
         stt_up=True,
         llm_up=True,
